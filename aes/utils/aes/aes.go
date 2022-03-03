92c7ae3bfa73e19218d145cfc39010a116179d26
mr-houzi <ghosthouzi@foxmail.com>
2022-03-03 19:36:47 +0800

    aes demo

---
+++ aes/utils/aes/aes.go
@@ -0,0 +1,60 @@
+package aes
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func AesEncode(plaintext []byte, encryptionKey []byte, iv []byte) ([]byte, error) {
+	//bPlaintext := PKCS5Padding(plaintext, aes.BlockSize)
+	bPlaintext := PKCS5Padding(plaintext)
+	block, err := aes.NewCipher(encryptionKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, len(bPlaintext))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, bPlaintext)
+
+	return ciphertext, err
+}
+
+func AesDecode(cipherText []byte, encryptionKey []byte, iv []byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(cipherText, cipherText)
+
+	return PKCS7UnPadding(cipherText), nil
+}
+
+// PKCS7Padding [PKCS7 / PKCS5 填充算法](https://segmentfault.com/a/1190000019793040)
+// PKCS7Padding [三种填充模式的区别(PKCS7Padding/PKCS5Padding/ZeroPadding)](https://blog.csdn.net/xiongya8888/article/details/84947232)
+func PKCS7Padding(cipherText []byte, blockSize int) []byte {
+	// 填充的长度
+	paddingLen := blockSize - len(cipherText)%blockSize
+	// 待填充的字符串
+	paddingChar := []byte{byte(paddingLen)}
+	padText := bytes.Repeat(paddingChar, paddingLen)
+
+	return append(cipherText, padText...)
+}
+
+// PKCS5Padding pkcs5作为pkcs7的子集算法，概念上没有什么区别，只是在blockSize上固定为 8 bytes
+func PKCS5Padding(cipherText []byte) []byte {
+	return PKCS7Padding(cipherText, 8)
+}
+
+// PKCS7UnPadding PKCS7 和 PKCS5 去重填充是一个规则
+func PKCS7UnPadding(plantText []byte) []byte {
+	length   := len(plantText)
+	// 填充的长度，最后一个字节肯定为填充数据的长度
+	paddingLen := int(plantText[length-1])
+	return plantText[:(length - paddingLen)]
+}
---
+++ aes/utils/aes/aes_test.go
@@ -0,0 +1,98 @@
+package aes
+
+import (
+	crand "crypto/rand"
+	"math/rand"
+	"encoding/hex"
+	"fmt"
+	"testing"
+	"time"
+)
+
+// go test -run=TestAesEncode -v
+func TestAesEncode(t *testing.T) {
+	//// 生成密钥
+	//key, err := GenerateRandomBytes(32)
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//// 生成偏移量
+	//iv, err := GenerateRandomBytes(16)
+	//if err != nil {
+	//	t.Error(err)
+	//}
+
+	// 生成对称密钥
+	key := GenrateRandomString(32)
+	// 生成偏移量
+	iv := GenrateRandomString(16) // iv 的长度需要 16 个字节，16个字母字符组成的字符串等于16字节（即[]byte 长度位16）
+
+	// 待加密的明文
+	plainText := "hello world"
+
+	t.Log("plainText（明文）:", plainText)
+
+	t.Log("key:", fmt.Sprintf("%v", key))
+	t.Log("iv:", fmt.Sprintf("%v", iv))
+
+	ciphertext, err := AesEncode([]byte(plainText), []byte(key), []byte(iv))
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log("ciphertext byte:", ciphertext)
+	t.Log("ciphertext:", string(ciphertext))
+	t.Log("ciphertext hex(加密后的密文16进制表示):", fmt.Sprintf("%x", ciphertext))
+}
+
+func TestAesDecode(t *testing.T) {
+	ciphertextHex := "ae5c4b54f33904d0dddcf2bbccdb2c02"
+	ciphertextByte, err := hex.DecodeString(ciphertextHex)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// 对称密钥
+	key := "IRCMQGSDPDVVYYJVVJLULIJAXBNFPHYT"
+	// 向量
+	iv := "IRCMQGSDPDVVYYJV"
+
+	plaintextByte, err := AesDecode(ciphertextByte, []byte(key), []byte(iv))
+	if err != nil {
+		t.Error(err)
+	}
+
+	plaintext := string(plaintextByte)
+	t.Log("解密结果：", plaintext)
+
+	if plaintext == "hello world" {
+		t.Log("eq")
+	} else {
+		t.Error("not eq")
+	}
+}
+
+// 生成随机字节
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := crand.Read(b)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// 生成全大写字母的随机字符串
+func GenrateRandomString(len int) string {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		b := r.Intn(26) + 65
+		bytes[i] = byte(b)
+	}
+	return string(bytes)
+}
